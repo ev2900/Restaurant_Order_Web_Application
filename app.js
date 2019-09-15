6717a36617ce200bf6bbd48a02e199af5338d7ce
Christopher Sharkey <ev2900@gmail.com>
2019-09-15 17:23:12 -0400

    Changes to home page view engine

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
 
+// View Engine + Url encoding
+app.set("views", __dirname + "/views");
+app.set('view engine', 'ejs');
+
+// routes
 app.get('/', function (req, res) {
-  response.sendFile('views/index.html');
+  res.render("index.ejs");
 })
 
 app.get('/api/test', function (req, res) {
   res.send('{"test":"successful"}')
 })
- 
+
+// open on port 80
 app.listen(80)
 console.log("application is running ...")
