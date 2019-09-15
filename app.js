efdeaa4cc46b25242bb18b694cac5e540034aac2
Christopher Sharkey <ev2900@gmail.com>
2019-09-15 12:52:43 -0400

    Added API test route

--- app.js
+++ app.js
@@ -4,5 +4,10 @@ const app = express()
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+
+app.get('/api/test', function (req, res) {
+  res.send('{"test":"successful"}')
+})
  
 app.listen(80)
+console.log("application is running ...")
