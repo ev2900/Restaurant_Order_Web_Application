e8a3252f1eaeaebc834269f65e37d08583e0f8e9
IceCreamEngineer <mpistacchio77@gmail.com>
2019-10-16 16:11:44 -0400

    US-15 completed!

---
+++ views/js/edit_menu_item_display.js
@@ -0,0 +1,47 @@
+$(function (){
+       
+    // parse cookie
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    var contact_name = ca[0].replace("name=","")
+    var contact_phone = ca[1].replace(" phone=","") 
+
+    var $cart = $('#cart');
+
+    var person = JSON.stringify({
+        "CONTACT_NAME": contact_name,
+        "CONTACT_PHONE": contact_phone
+    });
+    
+    $.ajax({
+        type: 'POST',
+        headers: { 
+            'Content-Type': 'application/json'
+        },
+        url: 'https://prod-12.centralus.logic.azure.com:443/workflows/623fba687c8a4d9983b03c59c34df6a7/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=uWI99lT_66N06ix1sNPExq1WHqQ_VS4f57_msIQCSRA',
+        data: person,
+        success: function(resp){
+            
+            var html = "";
+
+            $.each(resp.Table1, function(i, each) {
+ 
+                html += '<tr> <td> <br> </td> </tr>';
+                html += '<tr> <td> <img src="' + each.IMG_URL + '" alt="Pizza"> </td> </tr>';
+                html += '<tr> <td> ' + each.NAME + ' </td> </tr>';
+                html += '<tr> <td> Price: $' + each.PRICE + ' </td> </tr>'; 
+                html += '<tr> <td> <input id="edit" class="btn-default submit" type="submit" value="Edit"> </td> </tr>';
+                html += '<tr> <td> <input id="delete' + each.ITEM_ID + '" class="btn-default submit" type="submit" value="Delete"> </td> </tr>';
+    
+            });
+
+            // console.log(html);
+            $cart.html(html);
+
+        },
+        error: function() {
+            alert('error');
+        }
+    });
+
+});
