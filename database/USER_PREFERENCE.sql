fcde640eb90373725ede3c4dc47bed96b9247c7a
Christopher Sharkey <ev290@DESKTOP-NS7NCF5.northamerica.corp.microsoft.com>
2019-11-14 20:04:49 -0500

    data base changes

---
+++ database/USER_PREFERENCE.sql
@@ -0,0 +1,7 @@
+CREATE TABLE USER_PREFERENCE (
+	[NAME] nvarchar(max),
+	[PHONE_NUMBER] nvarchar(max),
+	[PREFERENCE_1] nvarchar(max),
+	[PREFERENCE_2] nvarchar(max),
+	[PREFERENCE_3] nvarchar(max)
+)
